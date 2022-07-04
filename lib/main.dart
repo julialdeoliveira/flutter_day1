523ba22b4cefea43db3c6414ba05da0534d3f9dc
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-04 14:26:07 -0300

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
+    //método que vai inflar noss app (executar)
     MaterialApp(
-      home: HomePage(),
+      //Um widget que já vem com muitas configurações prontas
+      home:
+          HomePage(), //Setando que a minha home (tela inicial) será uma homePage
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
 
 class HomePage extends StatelessWidget {
+  //Classe StatelessWidget (sem estado)
   @override
-  Widget build(BuildContext) {
+  Widget build(BuildContext context) {
+    //método que vai buildar toda vez nossa view passando o context
+    // (ajuda a identificar a nossa árvore) retornando um widget
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text('Olá mundo')),
+      //esqueleto do app, sempre interessanter ter 1 em cada Page
+      appBar: AppBar(), // Widget do próprio Flutter para AppBar
+      body: const Center(
+        //Widget para centralizar dentro do espaço disponível
+        child: Text('Olá mundo'), //Widget para mostrar um texto na tela
+      ),
     );
   }
 }
